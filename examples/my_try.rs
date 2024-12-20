d032a59c5c2206b964bfc1c16384c965ffdc4e47
Vincent <andyli386@gmail.com>
2024-12-20 15:18:36 +0800

    refactor: change crate to proc-macro

--- examples/my_try.rs
+++ examples/my_try.rs
@@ -1,6 +1,14 @@
 use anyhow::{anyhow, Result};
-use macros::my_try;
 
+#[macro_export]
+macro_rules! my_try {
+    ($expr:expr) => {
+        match $expr {
+            Ok(val) => val,
+            Err(err) => return Err(err.into()),
+        }
+    };
+}
 fn main() -> Result<()> {
     // let ret = f3(f2(f1("hello")?)?)?;
 
